41f1d10853b4814ae564a774058de7c65e82149e
kassem00 <kamsdonga@gmail.com>
2024-02-24 12:10:20 +0000

    au

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #!/usr/bin/pup
 # Using Puppet, install flask from pip3.
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['werkzeug'],
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
