4ccdeea44b8eaf4f8f3b112d8db5c268702df188
kassem00 <kamsdonga@gmail.com>
2024-03-04 18:30:52 -0800

    pupet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+#automate the task of creating a custom HTTP
+exec { 'setup-nginx':
+  command  => 'apt-get -y update && \
+      apt-get -y install nginx && \
+          sudo sed -i "/listen 80 default_server\\
+    /a add_header X-Served-By $HOSTNAME;" \
+        /etc/nginx/sites-available/default && \
+	    service nginx restart',
+	      provider => shell,
+	      }
