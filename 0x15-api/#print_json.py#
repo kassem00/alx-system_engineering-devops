634dfae9f72673a35bb1f045f5e2bc834c005272
kassem00 <kamsdonga@gmail.com>
2024-04-24 01:20:33 +0000

    au

---
+++ 0x15-api/#print_json.py#
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+under construction module for healthy json debuging
+"""
+import json
+
+def pri(data:dict={}):
+    """ pretty print json for healthy debuging """
+    print(f"{json.dumps(data, indent=4)}")
--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 """
 using this REST API,
-for a given employee ID, 
+for a given employee ID,
 returns information about his/her TODO list progress
 """
 import requests
+import sys
+
+if __name__ == "__main__":
+    in_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(in_id)
+    user = requests.get(url).json()
+    todos = requests.get(url + "/todos").json()
+    num_of_tasks = len(todos)
+    count = 0
+    completed_tasks = []
+
+    for i in range(num_of_tasks):
+        if todos[i]['completed'] is True:
+            count += 1
+            completed_tasks.append(todos[i]['title'])
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user['name'], count, num_of_tasks))
+    for task in completed_tasks:
+        print("     " + task)
