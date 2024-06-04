670dd43d59c584119b41684749ff50ade616ec4f
kassem00 <kamsdonga@gmail.com>
2024-06-04 22:45:05 +0000

    au

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,25 @@
-import requests
-import sys
-"""
-Write a function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit
-is given, the function should return 0.
-"""
+#!/usr/bin/python3
+"""Querying the Reddit API to return the number of subscribers for a given subreddit"""
 
+import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
-
+    """Read Reddit API and return the number of subscribers"""
+    headers = {'User-Agent': 'Python:reddit.subscriber.counter:v1.0 (by /u/yourusername)'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
             return 0
-        data = response.json()
-        return data['data']['subscribers']
     except requests.RequestException:
         return 0
 
-
 if __name__ == '__main__':
+    import sys
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
