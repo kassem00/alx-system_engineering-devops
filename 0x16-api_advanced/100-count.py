f1eedd332c6d273127a40976ed27bfee69f40184
kassem00 <kamsdonga@gmail.com>
2024-06-07 14:33:27 +0000

    au

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,8 +3,9 @@
 Querying the Reddit API to return the number of subscribers a given subreddit
 """
 
-import requests
 import sys
+import requests
+
 
 def number_of_subscribers(subreddit):
     """
@@ -17,23 +18,10 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers to the subreddit, or 0 if not subreddit.
     """
-    headers = {
-        'User-Agent': 'Python:reddit.subscriber.counter:v1.0 (by /u/username)'
-    }
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
+    headers = {'User-Agent': 'ka_reddit'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+    if response.status_code != 200:
+        return (0)
+    return (response.json().get("data").get("subscribers"))
