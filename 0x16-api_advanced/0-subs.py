b7e72c753b7afdc9874e3304e5eff9bec4d80fb9
kassem00 <kamsdonga@gmail.com>
2024-06-08 18:32:12 +0000

    au

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,10 +22,3 @@ def number_of_subscribers(subreddit):
 
     except requests.RequestException:
         return 0
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
