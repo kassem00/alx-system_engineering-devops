5d04e9370c181e0e9c5204c602053b8e58e4d31e
kassem00 <kamsdonga@gmail.com>
2024-06-09 09:36:53 +0000

    au

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix_apache_500_error
 
 exec { 'fix_apache_500_error':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   provider => 'shell',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
